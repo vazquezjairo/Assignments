0787fbc11bff31805c2e8be8c68d5dc88a34678f
vazquezjairo <91754203+vazquezjairo@users.noreply.github.com>
2021-10-19 13:26:38 -0700

    Add files via upload

---
+++ Likes/script.js
@@ -0,0 +1,23 @@
+var count1 = 9;
+var countElement1 = document.querySelector("#first-post");
+
+function add1() {
+    count1++;
+    countElement1.innerText = count1 + " Like(s)";
+}
+
+var count2 = 12;
+var countElement2 = document.querySelector("#second-post");
+
+function add2() {
+    count2++;
+    countElement2.innerText = count2 + " Likes(s)";
+}
+
+var count3 = 9;
+var countElement3 = document.querySelector("#third-post");
+
+function add3() {
+    count3++;
+    countElement3.innerText = count3 + " Likes(s)";
+}
